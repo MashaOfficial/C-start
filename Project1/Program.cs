192ba3eed131f624b8e16e603948f15fdd2ee481
Maria Chavalakh <masha_chavalah@mail.ru>
2017-10-26 01:02:22 +0300

    first commit

---
+++ Project1/Program.cs
@@ -0,0 +1,26 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_1
+{
+	class MainClass
+	{
+		static void Main(string[] args)
+		{
+			// Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello stranger. How can  I call you?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name + "!");
+			Console.BackgroundColor = ConsoleColor.White;
+			Console.ForegroundColor = ConsoleColor.DarkRed;
+			Console.WriteLine("New string");
+			Console.ResetColor();
+			Console.Beep();
+			Console.WriteLine(Console.CapsLock);
+			Console.WriteLine(Console.NumberLock);
+		}
+	}
+}
---
+++ Project1/Project1/Program_3.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Project3
+{
+    class MainClass
+    {
+        Console.WriteLine("Your fibbonacci number:\n");
+i       int number = Convert.ToInt32(Console.ReadLine());
+        if (number > 48)
+            {
+                Console.WriteLine("Too long number");
+                
+            }
+        else if (number< 1)
+            {
+                Console.WriteLine("Too small number\n");
+            }
+         else
+            {
+                int perv = 1;
+                int vtor = 1;
+                int sum = 0;
+
+                int j = 2;
+                while (j <= number)
+                {
+                    sum = perv + vtor;
+                    perv = vtor;
+                    vtor = sum;
+                    j++;
+                }
+                Console.WriteLine("Под номером " + number + " в ряде Фибоначчи стоит число " + perv); 
+            }
+    }
+}
+
+
